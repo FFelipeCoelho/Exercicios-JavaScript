3e60cf51f4d559824044ba376852e6671681735e
Felipe Coelho <102245077+FFelipeCoelho@users.noreply.github.com>
2026-09-01 19:46:43 -0300

    Rename exercicio058-Desconto_Produto.js to exercicio058-descontoProduto.js

--- exercicio058-Desconto_Produto.js
+++
@@ -1,5 +0,0 @@
-let
-    produto=200,
-    desconto=30
-
-console.log(`Nosso produto de ${produto} reais está em 15% de desconto, totalizando: R$ ${produto-desconto}`)
---
+++ exercicio058-descontoProduto.js
@@ -0,0 +1,5 @@
+let
+    produto=200,
+    desconto=30
+
+console.log(`Nosso produto de ${produto} reais está em 15% de desconto, totalizando: R$ ${produto-desconto}`)
