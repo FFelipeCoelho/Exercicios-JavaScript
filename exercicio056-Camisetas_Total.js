6a14452f9ab91429f9ef0004d85a796bec695788
Felipe Coelho <102245077+FFelipeCoelho@users.noreply.github.com>
2026-09-01 19:46:23 -0300

    Rename exercicio056-Camisetas_Total.js to exercicio056-camisetasTotal.js

--- exercicio056-Camisetas_Total.js
+++
@@ -1,5 +0,0 @@
-let
-    camiseta=50.00,
-    quantidade=11
-
-console.log(`${quantidade} camisetas custam R$ ${camiseta*quantidade}`)
---
+++ exercicio056-camisetasTotal.js
@@ -0,0 +1,5 @@
+let
+    camiseta=50.00,
+    quantidade=11
+
+console.log(`${quantidade} camisetas custam R$ ${camiseta*quantidade}`)
