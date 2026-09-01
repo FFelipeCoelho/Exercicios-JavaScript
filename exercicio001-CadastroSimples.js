3727e053d7862fc035ed0ac525caa96dbed5e96b
Felipe Coelho <102245077+FFelipeCoelho@users.noreply.github.com>
2026-09-01 19:34:50 -0300

    Rename exercicio001-CadastroSimples.js to exercicio001-cadastroSimples.js

--- exercicio001-CadastroSimples.js
+++
@@ -1,3 +0,0 @@
-const nome="Felipe", idade="24",cidade="São Paulo";
-
-console.log("Olá, eu sou",nome,"e tenho",idade,"anos, moro em",cidade,"e gosto de videogames!")
---
+++ exercicio001-cadastroSimples.js
@@ -0,0 +1,3 @@
+const nome="Felipe", idade="24",cidade="São Paulo";
+
+console.log("Olá, eu sou",nome,"e tenho",idade,"anos, moro em",cidade,"e gosto de videogames!")
